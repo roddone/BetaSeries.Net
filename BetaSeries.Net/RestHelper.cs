8d8245f0cec878de4d93be96dee96fe2d1ae8aa0
Romain ODDONE <romain.oddone@outlook.com>
2018-04-19 15:47:22 +0200

    add method to hash MD5 password

--- BetaSeries.Net/RestHelper.cs
+++ BetaSeries.Net/RestHelper.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BetaSeries.Net
@@ -49,6 +51,26 @@ namespace BetaSeries.Net
         #endregion Public Constructors
 
         #region Public Methods
+
+        public static string HashPassword(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            using (MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hash = md5.ComputeHash(inputBytes);
+
+                // step 2, convert byte array to hex string
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
 
         public static async Task<dynamic> Delete<T>(dynamic parameters) where T : class
         {
